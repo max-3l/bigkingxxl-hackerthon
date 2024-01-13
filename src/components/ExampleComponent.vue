89ead9efc599c9690d08dac7275a05a56ec00d9f
Maximilian Schulze <49536186+max-3l@users.noreply.github.com>
2024-01-13 15:48:42 +0100

    🍔 initial commit

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-form @submit="onSubmit" v-show="showInput" class="input">
+    <q-input rounded outlined v-model="text" label="Prompt" />
+  </q-form>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+} from 'vue';
+
+
+export default defineComponent({
+  name: 'ExampleComponent',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    active: {
+      type: Boolean
+    }
+  },
+  setup() {
+    return {
+      text: ref(''),
+      showInput: ref(true)
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      console.log(this.text);
+      this.showInput = false;
+    }
+  }
+});
+</script>
+
+<style>
+.input {
+  width: 90%
+}
+</style>
